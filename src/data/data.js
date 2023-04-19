477cf9e69c51cd3fe5e8cc96a4d687e610358f6e
AKHIL KOSHLE <akhilshrinarayankoshle@AKHILs-MacBook-Air.local>
2023-04-19 14:51:59 +0530

    Initial Commit

---
+++ src/data/data.js
@@ -0,0 +1,52 @@
+import Dosa from "../images/dosa.jpg";
+import Chola from "../images/chhola.jpg";
+import Idli from "../images/idli.jpg";
+import MasalaDosa from "../images/masala.jpg";
+import Paneer from "../images/paneer.jpg";
+import Gujrati from "../images/gujrati.jpeg";
+
+export const MenuList = [
+  {
+    name: "Dosa",
+    price: "200",
+    description:
+      "As dosa’s greatness spreads across the globe, it’s quickly making a name for itself across different nationalities and culinary audiences",
+    image: Dosa,
+    //price: 200,
+  },
+  {
+    name: "Chola",
+    description:
+      " Chole stands for a spiced tangy chickpea curry and Bhatura is a soft and fluffy fried leavened bread. Chola Bhatura always make for a delicious and filling mea!",
+    image: Chola,
+    price: 250,
+  },
+  {
+    name: "Idli Sambhar",
+    description:
+      "Idli Sambar is a hearty, satisfying, comforting and a healthy meal of soft fluffy idlis served with savory, spiced and lightly tangy sambar ",
+    image: Idli,
+    price: 300,
+  },
+  {
+    name: "Masala Dosa",
+    description:
+      "a thin pancake in South Indian cuisine made from a fermented batter of ground black lentils and rice",
+    image: MasalaDosa,
+    price: 100,
+  },
+  {
+    name: "Paneer masala",
+    description:
+      "Paneer Butter Masala is one of India’s most popular paneer gravy recipes, and with good reason! Indian cottage cheese cubes are smothered in a creamy, lightly spiced tomato sauce that is downright delicious.",
+    image: Paneer,
+    price: 400,
+  },
+  {
+    name: "Gujrati thali",
+    description:
+      "A Gujarati thali typically comprises of one or two steamed or fried snacks called farsans, a green vegetable, a tuber or a gourd shaak (shaaks are main courses with vegetables and spices mixed together into a curry or a spicy dry dish)" ,
+    image: Gujrati,
+    price: 500,
+  },
+];
